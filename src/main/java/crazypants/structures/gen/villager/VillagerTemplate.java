a3d1710be5163732d47424f7270142ec60bc32af
CrazyPants <crazypants.mc@gmail.com>
2015-12-30 20:51:08 +0800

    Added documentation for villager templates

--- src/main/java/crazypants/structures/gen/villager/VillagerTemplate.java
+++ src/main/java/crazypants/structures/gen/villager/VillagerTemplate.java
@@ -5,41 +5,55 @@ import java.util.List;
 
 import com.google.gson.annotations.Expose;
 
+import crazypants.structures.api.AttributeDoc;
 import crazypants.structures.api.AttributeEditor;
 import crazypants.structures.api.ListElementType;
 import crazypants.structures.api.gen.IResource;
 import crazypants.structures.api.gen.IVillagerGenerator;
 import crazypants.structures.api.gen.WeightedTemplate;
+import crazypants.structures.gen.io.resource.StructureResourceManager;
 import net.minecraft.village.MerchantRecipe;
 
 public class VillagerTemplate implements IResource {
 
+  @AttributeDoc(text = "The unique name of the template. Corresponds to a file with the same name and " + StructureResourceManager.VILLAGER_EXT + " extension")
   private String uid;
 
+  @AttributeDoc(text = "The heigher the weight, the higher the chance the village building will generate. "
+      + "For example, a vanilla church and house has a wieght of 20 while a blacksmith has a weight of 15")
   @Expose
   private int weight;
 
+  @AttributeDoc(text = "The minimum number to generate per village")
   @Expose
   private int minGeneratedPerVillage;
 
+  @AttributeDoc(text = "The maximum number to generate per village")
   @Expose
   private int maxGeneratedPerVillage;
 
+  @AttributeDoc(text = "The template to use for generating the village building. One template is chosen for each building constructed.")
   @ListElementType(elementType = WeightedTemplate.class)
   @Expose
   private List<WeightedTemplate> plainsTemplates = new ArrayList<WeightedTemplate>();
 
+  @AttributeDoc(text = "The template to use for generating the village building in a desert. If none are added the plains templates will be used.")
   @ListElementType(elementType = WeightedTemplate.class)
   @Expose
   private List<WeightedTemplate> desertTemplates = new ArrayList<WeightedTemplate>();
 
+  @AttributeDoc(text = "The unique ID for the villager. Must not clash with other mods. New buildings can be added to a village "
+      + "without creating a new villager by setting thsi to -1.")
   @Expose
   private int villagerId;
-
-  @AttributeEditor(name="texture")
+  
+  @AttributeDoc(text = "The name of image used as the villagers texture. Images loaded from a config dir or zip file must be "
+      + "prepended with 'esresource:' For example, esresource:myImage.png.")
+  @AttributeEditor(name = "texture")
   @Expose
   private String texture;
 
+  @AttributeDoc(text = "Adds custum villager trades")
   @ListElementType(elementType = MerchantRecipeWrapper.class)
   @Expose
   private List<MerchantRecipeWrapper> trades = new ArrayList<MerchantRecipeWrapper>();
@@ -67,12 +81,12 @@ public class VillagerTemplate implements IResource {
     }
     return true;
   }
-  
+
   private boolean hasValidTemplate(List<WeightedTemplate> templates) {
     if(templates == null || templates.isEmpty()) {
       return false;
     }
-    for( WeightedTemplate tmpl : templates) {
+    for (WeightedTemplate tmpl : templates) {
       if(tmpl.getTemplate() == null) {
         return false;
       }
@@ -84,7 +98,7 @@ public class VillagerTemplate implements IResource {
     VillagerGenerator res = new VillagerGenerator(uid);
 
     for (WeightedTemplate tmpl : plainsTemplates) {
-      if(tmpl != null && tmpl.getTemplate() != null) {       
+      if(tmpl != null && tmpl.getTemplate() != null) {
         res.addPlainsTemplate(tmpl);
       }
     }
